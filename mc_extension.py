06f8437865dae2e20d0cff22409da74ec681651c
Johnny Chang <johnny@johnnychang.com>
2015-12-04 00:19:48 -0600

    Successfully parsed Markdown MCQ to HTML!

--- mc_extension.py
+++ mc_extension.py
@@ -1,9 +1,10 @@
 from __future__ import absolute_import
 from markdown.blockprocessors import BlockProcessor
 from markdown.extensions import Extension
+from markdown.util import etree
 
-MCQ_START = "@MCQ"
-MCQ_END = "@END"
+MCQ_START = "@MCQ\n"
+MCQ_END = "@END\n"
 
 class MCProcessor(BlockProcessor):
     """
@@ -15,8 +16,25 @@ class MCProcessor(BlockProcessor):
 
     def run(self, parent, blocks):
         raw_block = blocks.pop(0)
-        lines = [l.strip().rstrip(MCQ_END).lstrip(MCQ_START) for l in
-                 raw_block.split("\n")]
+        lines = [l.strip() for l in
+                 raw_block.rstrip(MCQ_END).lstrip(MCQ_START).split("\n")]
+
+        question_container = etree.SubElement(parent, 'div')
+        question_container.set("class", "question-container")
+
+        question = etree.SubElement(question_container, 'div')
+        question.set("class", "question")
+
+        question_text = etree.SubElement(question, "h3")
+        question_text.text = lines[0]
+
+        choices = etree.SubElement(question, "ol")
+        choices.set("type", "A")
+        for choice in lines[1:-1]:
+            choice_elem = etree.SubElement(choices, "li")
+            choice_content = etree.SubElement(choice_elem, "a")
+            choice_content.set("href", "#")
+            choice_content.text = choice
         print lines
 
 class MCExtension(Extension):
