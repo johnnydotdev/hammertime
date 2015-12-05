cc402f532ea832827106f731987443bfba42a11b
Johnny Chang <johnny@johnnychang.com>
2015-12-05 14:50:57 -0600

    Cleaned up project directory.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name = 'hammertime',
+    packages = ['hammertime'],
+    version = '0.1',
+    description = 'A GFM to HTML parser that adds interactive Multiple Choice question functionality.',
+    author = 'Johnny Chang',
+    author_email = 'johnny@johnnychang.com',
+    url = 'https://github.com/goldcase/hammertime',
+    keywords = ['multiple', 'choice', 'question', 'markdown', 'html', 'converter', 'gfm', 'github', 'flavored'],
+    classifiers = [],
+)
